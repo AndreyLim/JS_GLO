00b895efb1070bb13dc9fc8948cd96f751c6a005
Andrew <skazlipop.88@mail.ru@>
2021-01-29 16:34:04 +0300

    lesson02 complite

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,10 +1,21 @@
-let money; 
-let income; 
-let addExpenses; 
-let deposit; 
-let mission; 
-let period;
+let money = 40000; 
+let income = 'фриланс'; 
+let addExpenses = 'Коммуналка, Интернет, Аренда репточки, Кредит'; 
+let deposit = 1; 
+let mission = 300000; 
+let period = 6;
 
-alert('Text');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('Любой текст');
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+let addExpensesLow = addExpenses.toLowerCase();
+console.log(addExpensesLow.split(', '));
+
+let budgetDay = money/30;
+console.log(budgetDay);
